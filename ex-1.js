d045acab93be6cd574c7f00d6750e9a80cc13941
bp3-Tiger <sataporn.thirasaroj@gmail.com>
2024-04-20 15:54:11 +0700

    bp3-Tiger

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,16 @@
 // Start coding here
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function devide(a, b) {
+    return a / b;
+}
